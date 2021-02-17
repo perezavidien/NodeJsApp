4a32ce5afd4e765ea5934b5ea9f81907a456de52
Avi Perez <perezavidien@gmail.com>
2021-02-18 02:27:53 +0800

    use main.js as startup file

--- main.js
+++ main.js
@@ -3,9 +3,12 @@
 // LoanAmount * (1 + (LoanTerm *(InterestRate / 100))) / LoanTerm
 // use this command to run -> node main.js --loanTerm=24 --bankName='bpi' --loanAmount=10000
 
+'use strict';
 
 const { argv } = require('yargs');
 
 console.log(argv);
+
+console.log('Hello world');
 
 

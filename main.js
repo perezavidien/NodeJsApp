32f90b217f65e5ca3cc912224f074b1af8e82eeb
Avi Perez <perezavidien@gmail.com>
2021-02-22 22:54:25 +0800

    calc implementation

--- main.js
+++ main.js
@@ -5,14 +5,83 @@
 
 'use strict';
 
-const { argv } = require('yargs');
+const { argv, strict } = require('yargs');
 
-// console.log(argv);
+class Bank {
+    constructor(loanAmount, interestRate) {
+        this.loanAmout = loanAmount;
+        this.interestRate = interestRate;
+    }
 
-const bank = argv.bankName;
-const amount = argv.loanAmount;
-const term = argv.loanTerm;
+    getMonthlyInstallment(loanTerm) {
+        // console.log(parseInt(this.loanAmout));
+        // console.log(parseFloat(this.interestRate));
+        // console.log(parseInt(loanTerm));
+        return (this.loanAmout + (this.loanAmout * this.interestRate * loanTerm)) / loanTerm;
+    }
+}
 
-// console.log(bank);
-// console.log(amount);
-// console.log(term);
+class BDO extends Bank {
+    constructor(loanAmount) {
+        super(loanAmount, 0.017);
+        this.loanAmout = loanAmount;
+    }
+}
+
+class BPI extends Bank {
+    constructor(loanAmount) {
+        super(loanAmount, 0.012);
+        this.loanAmout = loanAmount;
+    }
+}
+
+class Metrobank extends Bank {
+    constructor(loanAmount) {
+        super(loanAmount, 0.015);
+        this.loanAmout = loanAmount;
+    }
+}
+
+class LoanCalculator {
+    constructor(bankName, loanAmout, loanTerm) {
+        this.bankName = bankName;
+        this.loanAmout = loanAmout;
+        this.loanTerm = loanTerm;
+    }
+
+    getMonthlyInstallment() {
+        let myBank;
+        switch (this.bankName) {
+            case 'bdo':
+                myBank = new BDO(this.loanAmout);
+                break;
+            case 'bpi':
+                myBank = new BPI(this.loanAmout);
+                break;
+            case 'metrobank':
+                myBank = new Metrobank(this.loanAmout);
+                break;
+            default:
+                console.log('Your bank is not supported')
+                break;
+        }
+
+        if (!myBank)
+            return false;
+
+        return myBank.getMonthlyInstallment(this.loanTerm);
+    }
+}
+
+const bankParam = argv.bankName;
+const amountParam = argv.loanAmount;
+const termParam = argv.loanTerm;
+
+if (typeof (bankParam) !== 'string' || typeof (amountParam) !== 'number' || typeof (termParam) !== 'number')
+    console.log('ERROR: Invalid parameters entered.');
+else {
+    const myCalc = new LoanCalculator(bankParam, amountParam, termParam);
+    const mi = myCalc.getMonthlyInstallment();
+    if (mi)
+        console.log(`Monthly Installment:  ${mi}`);
+}

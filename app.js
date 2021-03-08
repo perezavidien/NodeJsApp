d9ea7f735838d15de8163902db0b1380b741f92b
Avi Perez <perezavidien@gmail.com>
2021-03-08 10:58:18 -0600

    added notes

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ if (typeof (nameParam) !== 'string' || typeof (pathParam) !== 'string')
     console.log('ERROR: Invalid parameters entered.');
 else {
     //do stuff here
-    console.log(nameParam);
-    console.log(pathParam);
+    console.log('Watching path: ' + pathParam);
+
+    const fileName = pathParam.slice(pathParam.lastIndexOf('\\') + 1, pathParam.length);
+    console.log(fileName);
+    //todo monitor the file changes
+    //when updated, check if name exists
+    //set nameExists if found, nt case sensitive
+    let nameExists = true;
+    if (nameExists) {
+        //todo windows toast message
+        console.log('Your name was mentioned on file: ' + fileName);
+    }
 }

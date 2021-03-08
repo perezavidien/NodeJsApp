3d8e34f48a67d856cf5393208895aa49be379ca5
Avi Perez <perezavidien@gmail.com>
2021-03-08 11:03:13 -0600

    used toast

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const { argv, strict } = require('yargs');
+const notifier = require('node-notifier');
 
 const nameParam = argv.name;
 const pathParam = argv.path;
@@ -13,13 +14,17 @@ else {
     console.log('Watching path: ' + pathParam);
 
     const fileName = pathParam.slice(pathParam.lastIndexOf('\\') + 1, pathParam.length);
-    console.log(fileName);
+    //console.log(fileName);
+
     //todo monitor the file changes
     //when updated, check if name exists
     //set nameExists if found, nt case sensitive
     let nameExists = true;
     if (nameExists) {
-        //todo windows toast message
-        console.log('Your name was mentioned on file: ' + fileName);
+        notifier.notify({
+            title: 'File Watcher',
+            message: 'Your name was mentioned on file: ' + fileName + '!'
+          });
+        console.log('Your name was mentioned on file: ' + fileName + '!');
     }
 }

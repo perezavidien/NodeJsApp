b4ff6635299b123f893b54501dc994663f5853a7
Avi Perez <perezavidien@gmail.com>
2021-03-08 12:04:07 -0600

    listener and emitter

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 
+
 'use strict';
 
 const { argv, strict } = require('yargs');
 const notifier = require('node-notifier');
+const { EventEmitter } = require("events");
+
+class FileWatcher extends EventEmitter {
+    constructor() {
+        super();
+    }
+
+    watchfolder(name, path) {
+        //console.log('name found on file called');
+        //should watch the path
+        const fileName = path.slice(path.lastIndexOf('\\') + 1, path.length);
+
+        //implement file watcher logichere
+        const nameFound = true;
+
+        if (nameFound)
+            this.emit("nameFoundOnFile", fileName);
+    }
+}
 
 const nameParam = argv.name;
 const pathParam = argv.path;
@@ -10,21 +30,35 @@ const pathParam = argv.path;
 if (typeof (nameParam) !== 'string' || typeof (pathParam) !== 'string')
     console.log('ERROR: Invalid parameters entered.');
 else {
-    //do stuff here
-    console.log('Watching path: ' + pathParam);
+    console.log(`Watching path: ${pathParam}`);
 
-    const fileName = pathParam.slice(pathParam.lastIndexOf('\\') + 1, pathParam.length);
-    //console.log(fileName);
+    //wait for updates here
+    const fileWatcher = new FileWatcher(nameParam, pathParam);
+    //const fileWatcher = new EventEmitter();
+    //const fileName = pathParam.slice(pathParam.lastIndexOf('\\') + 1, pathParam.length);
 
-    //todo monitor the file changes
-    //when updated, check if name exists
-    //set nameExists if found, nt case sensitive
-    let nameExists = true;
-    if (nameExists) {
+    fileWatcher.on("nameFoundOnFile", fileName => {
+        //console.log('inside nameFoundOnFile');
+        fileWatcher.emit("openToastNotification", fileName);
+        fileWatcher.emit("printToConsole", fileName);
+    });
+
+    fileWatcher.on("openToastNotification", fileName => {
+        //console.log('inside openToastNotification');
         notifier.notify({
             title: 'File Watcher',
-            message: 'Your name was mentioned on file: ' + fileName + '!'
-          });
-        console.log('Your name was mentioned on file: ' + fileName + '!');
-    }
+            message: `Your name was mentioned on file: ${fileName}!`
+        });
+    });
+
+    fileWatcher.on("printToConsole", fileName => {
+        //console.log('inside printToConsole');
+        console.log(`Your name was mentioned on file: ${fileName}!`);
+    });
+
+    fileWatcher.on("error", () => {
+        console.log('One or more errors occured.');
+    });
+
+    fileWatcher.watchfolder(nameParam, pathParam);
 }
